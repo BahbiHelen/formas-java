8d3ff7ea27c286477670cbce3db4e33449dc5078
Bahbihelen <bahbarahelen@gmail.com>
2023-08-03 16:45:23 -0300

    Initial commit

---
+++ c.js
@@ -0,0 +1,41 @@
+function Area () {
+    let r=Number(prompt("Informe o valor do raio do comprimento"))
+    let y=Number(prompt("Informe o valor do raio do largura"))
+    let resultado=r*y
+    alert("o resultado da área é "+resultado)
+    document.getElementById('1_ret').innerHTML=resultado
+}
+
+function Areab () {
+    let l1=Number(prompt("Informe o valor l1"));
+    let l2=Number(prompt("Informe o valor l2"));
+    let resultado=l1*l2
+    alert("O resultado da área é"+resultado+"m³")
+    document.getElementById('2_ret').innerHTML=resultado
+}
+
+function Areac () {
+    let l1=Number(prompt("Informe o valor l1"));
+    let l2=Number(prompt("Informe o valor l2"));
+    let resultado=l1*l2
+    alert("O resultado da área é"+resultado+"m³")
+    document.getElementById('3_ret').innerHTML=resultado
+}
+
+function Areac () {
+    let l1=Number(prompt("Informe o valor l1"));
+    let l2=Number(prompt("Informe o valor l2"));
+    let resultado=l1*l2
+    alert("O resultado da área é"+resultado+"m³")
+    document.getElementById('4_ret').innerHTML=resultado
+}
+
+function Aread () {
+    let l1=Number(prompt("Informe o valor l1"));
+    let l2=Number(prompt("Informe o valor l2"));
+    let l3=Number(prompt("Informe o valor l3"));
+    let resultado=l1*l2*l3
+    alert("O resultado da área é"+resultado+"m³")
+    document.getElementById('5_ret').innerHTML=resultado
+}
+
